e98aac53875f94359fcaeed7043c7b5710cacb35
dr1602 <david.arangelg@gmail.com>
2024-01-30 09:46:21 -0600

    101 30Ene24

---
+++ main.py
@@ -0,0 +1,29 @@
+print('Hola Mundo, esto es Python.')
+
+print('Hola, soy Python, y escribo en tu computadora.')
+
+# Comentario entre lineas, operaciones matematicas
+
+#print(12 + 5)
+print(10 - 5 / 5)
+print(2 * 10 % 5)
+print(9 % 4)
+#    print(8 / 2)
+
+# Esto es un comentario
+# No podemos escribir un comentario de varias lineas, pero si de la siguiente manera:
+
+'''
+    Este es un comentario
+    que esta hecho en 
+    varias lineas.
+'''
+
+"""
+Esta
+es
+otra
+forma
+de
+comentar
+"""
