5f4988006e86ee2737fdae22c37fd0b2f6622b11
dr1602 <david.arangelg@gmail.com>
2024-03-04 12:49:35 -0600

    Variables 04Mar24

---
+++ 01_print.py
@@ -0,0 +1,10 @@
+print('Hola Mundo, esto es el archivo 01.')
+
+print('Hola, soy Python, y escribo en tu computadora.')
+
+# Comentario entre lineas, operaciones matematicas
+
+#print(12 + 5)
+print(10 - 5 / 5)
+print(2 * 10 % 5)
+print(9 % 4)
---
+++ 02_vars.py
@@ -0,0 +1,35 @@
+print('Archivo 02')
+
+# esta es una variable
+my_name = 'Dave'
+
+print(my_name)
+print('Hola, mi nombre es: ' + my_name)
+
+# esta es una variable con un numero
+
+my_age = 15
+
+print(my_age)
+
+#se pueden actaulizar las variables asi
+
+my_name = 'Lucia'
+
+print(my_name)
+print('El nuevo nombre es: ' + my_name)
+
+# tambien podemos identicarlas para momnetos especificos
+print("Aqui cambio: ", my_name)
+
+# input para pedir valores a las y los usarios
+
+my_name = input("What's your name?")
+
+print('the name that the user has provided is: ', my_name)
+
+# ahora pediremos la edad
+
+my_age = input("How old are you?")
+
+print('the age that the user has provided is: ', my_age)
