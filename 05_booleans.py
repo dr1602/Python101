9fd406386731a62f8ed9ca467cf0de8d36356c2d
dr1602 <david.arangelg@gmail.com>
2024-04-04 14:45:59 -0600

    Booleans 4Abril24

---
+++ 05_booleans.py
@@ -0,0 +1,13 @@
+is_single = True;
+print(type(is_single));
+
+is_single = False;
+print(is_single)
+
+# Transformar al valor contrario
+print(not True)
+print(not False)
+
+# Se puede transformar si no sabemos el valor al venir de un tercer, para transformar el valor
+is_single = not is_single;
+print(is_single)
