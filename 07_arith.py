0d153eb419f87dd11d5f810103910be4956756f6
dr1602 <david.arangelg@gmail.com>
2024-04-09 13:54:02 -0600

    Operadores 07_arith 9abril24

---
+++ 07_arith.py
@@ -0,0 +1,35 @@
+print(10 + 10);
+print(10 - 10);
+print(10 * 2);
+print(10 / 3);
+
+# Puede cambiar si utilizamos strings
+
+print('Hola' + ' mundo');
+print('Hola' * 3);
+
+# Operador de modulo, que da igual al residuo
+
+# Residuo cero por devision exacta
+print(10 % 2);
+
+# Residuo existente, por ser division inexacta
+print(10 % 3);
+
+# Operador de doble division // que nos da solo el entero resultante de una division
+print(10 // 3);
+
+# Operador de potencia
+print(10 ** 3);
+
+# Operacion mas compleja
+
+print(2 ** 3 + 3 - 7 / 1 // 4);
+
+# Este es el proceso de resolucion
+print(2 ** 3) # 8
+print(7 / 1 // 4) # 1
+print( 8 + 3 - 1) # 10
+
+## Tener cuidado con las divisiones como:
+print(10 / 0)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-age = input('Escribe tu edad => ');
-age = int(age);
-age += 10;
-print(type(age));
-print(f'Tu edad en 10 años será {age}')
+print(10 / 0)
