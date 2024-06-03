4a748adba954bfb240885312bd4e7440bfca15ec
dr1602 <david.arangelg@gmail.com>
2024-06-03 10:35:30 -0600

    Random 3Jun24

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,32 @@
-prompt = 'Este programa evalua si el numero de tu interes es par o impar'
+import random as rd
 
-print(prompt)
-numero = int(input('Ingrese el numero de su interes: '))
+user_option = input('Elige piedra, papel o tijera: ');
+game_options = ['piedra', 'papel', 'tijera'];
 
-if numero%2 == 0:
-    print('Su numero es par')
-elif numero%2 != 0:
-    print('Su numero es impar')
-else:
-    print('Existe algun tipo de error, disculpe los inconvenientes.')
+computer_option = rd.choice(game_options)
+
+print(f'La computadora ha escogido {computer_option}')
+
+if user_option == computer_option:
+    print('Esto es un empate, seleccionaron la misma opcion');
+elif user_option == 'piedra':
+    if computer_option == 'tijera':
+        print('Piedra gana a tijera');
+        print('El o la usuario ha ganado.');
+    else:
+        print('Papel gana a piedra');
+        print('La computadora ha ganado.');
+elif user_option == 'papel':
+    if computer_option == 'tijera':
+        print('Tijera gana a papel');
+        print('La computadora ha ganado.');
+    else:
+        print('Papel gana a piedra');
+        print('El o la usuario ha ganado.');
+elif user_option == 'tijera':
+    if computer_option == 'papel':
+        print('Tijera gana a papel');
+        print('El o la usuario ha ganado.');
+    else:
+        print('Piedra gana a tijera');
+        print('La computadora ha ganado.');
