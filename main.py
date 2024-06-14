2975a590fccb7d3903226abd552780f8f2c707d3
dr1602 <david.arangelg@gmail.com>
2024-06-14 11:31:25 -0600

    Dict 14jun24

--- main.py
+++ main.py
@@ -30,34 +30,82 @@ comentar
 
 import random as rd
 
+print('Este es el juego de piedra, papel o tijera.');
+
 user_option = input('Elige piedra, papel o tijera: ').lower();
-game_options = ['piedra', 'papel', 'tijera'];
+game_options = ('piedra', 'papel', 'tijera');
 
-computer_option = rd.choice(game_options)
-
-print(f'Has escogido {user_option}')
-print(f'La computadora ha escogido {computer_option}')
-
-if user_option == computer_option:
-    print('Esto es un empate, seleccionaron la misma opcion');
-elif user_option == 'piedra':
-    if computer_option == 'tijera':
-        print('Piedra gana a tijera');
-        print('El o la usuario ha ganado.');
+if user_option in game_options:
+    
+    computer_option = rd.choice(game_options)
+    
+    print(f'Has escogido {user_option}')
+    print(f'La computadora ha escogido {computer_option}')
+    
+    if user_option == computer_option:
+        print('Esto es un empate, seleccionaron la misma opcion');
+    elif user_option == 'piedra':
+        if computer_option == 'tijera':
+            print('Piedra gana a tijera');
+            print('El o la usuario ha ganado.');
+        else:
+            print('Papel gana a piedra');
+            print('La computadora ha ganado.');
+    elif user_option == 'papel':
+        if computer_option == 'tijera':
+            print('Tijera gana a papel');
+            print('La computadora ha ganado.');
+        else:
+            print('Papel gana a piedra');
+            print('El o la usuario ha ganado.');
+    elif user_option == 'tijera':
+        if computer_option == 'papel':
+            print('Tijera gana a papel');
+            print('El o la usuario ha ganado.');
+        else:
+            print('Piedra gana a tijera');
+            print('La computadora ha ganado.');
+    
+else:
+    
+    print('Esa opción no es valida, escoge de nuevo (tienes 2 oportunidades más).')
+    
+    i = 0
+    while i < 2:
+        i += 1
+        user_option = input('Elige piedra, papel o tijera: ').lower();
+        
+        if user_option in game_options:
+            break
+        
+    if user_option in game_options:
+        computer_option = rd.choice(game_options)
+    
+        print(f'Has escogido {user_option}')
+        print(f'La computadora ha escogido {computer_option}')
+        
+        if user_option == computer_option:
+            print('Esto es un empate, seleccionaron la misma opcion');
+        elif user_option == 'piedra':
+            if computer_option == 'tijera':
+                print('Piedra gana a tijera');
+                print('El o la usuario ha ganado.');
+            else:
+                print('Papel gana a piedra');
+                print('La computadora ha ganado.');
+        elif user_option == 'papel':
+            if computer_option == 'tijera':
+                print('Tijera gana a papel');
+                print('La computadora ha ganado.');
+            else:
+                print('Papel gana a piedra');
+                print('El o la usuario ha ganado.');
+        elif user_option == 'tijera':
+            if computer_option == 'papel':
+                print('Tijera gana a papel');
+                print('El o la usuario ha ganado.');
+            else:
+                print('Piedra gana a tijera');
+                print('La computadora ha ganado.');
     else:
-        print('Papel gana a piedra');
-        print('La computadora ha ganado.');
-elif user_option == 'papel':
-    if computer_option == 'tijera':
-        print('Tijera gana a papel');
-        print('La computadora ha ganado.');
-    else:
-        print('Papel gana a piedra');
-        print('El o la usuario ha ganado.');
-elif user_option == 'tijera':
-    if computer_option == 'papel':
-        print('Tijera gana a papel');
-        print('El o la usuario ha ganado.');
-    else:
-        print('Piedra gana a tijera');
-        print('La computadora ha ganado.');
+        print('Gracias por jugar.')
