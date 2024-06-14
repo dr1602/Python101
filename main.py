e62a8766f3b06c85729a6c664240911e11667cde
dr1602 <david.arangelg@gmail.com>
2024-06-14 16:06:49 -0600

    Finished Piedra Papel o Tijera

--- main.py
+++ main.py
@@ -1,86 +1,29 @@
-print('Hola Mundo, esto es Python.')
-
-print('Hola, soy Python, y escribo en tu computadora.')
-
-# Comentario entre lineas, operaciones matematicas
-
-#print(12 + 5)
-print(10 - 5 / 5)
-print(2 * 10 % 5)
-print(9 % 4)
-#    print(8 / 2)
-
-# Esto es un comentario
-# No podemos escribir un comentario de varias lineas, pero si de la siguiente manera:
-
-'''
-    Este es un comentario
-    que esta hecho en 
-    varias lineas.
-'''
-
-"""
-Esta
-es
-otra
-forma
-de
-comentar
-"""
-
 import random as rd
 
 print('Este es el juego de piedra, papel o tijera.');
 
-user_option = input('Elige piedra, papel o tijera: ').lower();
 game_options = ('piedra', 'papel', 'tijera');
 
-if user_option in game_options:
+rondas = 0
+
+user_victories = 0
+computer_victories = 0
+
+while True:
     
-    computer_option = rd.choice(game_options)
+    rondas += 1
     
-    print(f'Has escogido {user_option}')
-    print(f'La computadora ha escogido {computer_option}')
+    print('*' * 12)
+    print(f'Ronda {rondas}')
+    print('*' * 12)
     
-    if user_option == computer_option:
-        print('Esto es un empate, seleccionaron la misma opcion');
-    elif user_option == 'piedra':
-        if computer_option == 'tijera':
-            print('Piedra gana a tijera');
-            print('El o la usuario ha ganado.');
-        else:
-            print('Papel gana a piedra');
-            print('La computadora ha ganado.');
-    elif user_option == 'papel':
-        if computer_option == 'tijera':
-            print('Tijera gana a papel');
-            print('La computadora ha ganado.');
-        else:
-            print('Papel gana a piedra');
-            print('El o la usuario ha ganado.');
-    elif user_option == 'tijera':
-        if computer_option == 'papel':
-            print('Tijera gana a papel');
-            print('El o la usuario ha ganado.');
-        else:
-            print('Piedra gana a tijera');
-            print('La computadora ha ganado.');
-    
-else:
-    
-    print('Esa opción no es valida, escoge de nuevo (tienes 2 oportunidades más).')
-    
-    i = 0
-    while i < 2:
-        i += 1
-        user_option = input('Elige piedra, papel o tijera: ').lower();
-        
-        if user_option in game_options:
-            break
-        
+    user_option = input('Elige piedra, papel o tijera: ').lower();
+
+
     if user_option in game_options:
+        
         computer_option = rd.choice(game_options)
-    
+        
         print(f'Has escogido {user_option}')
         print(f'La computadora ha escogido {computer_option}')
         
@@ -90,22 +33,128 @@ else:
             if computer_option == 'tijera':
                 print('Piedra gana a tijera');
                 print('El o la usuario ha ganado.');
+                user_victories += 1;
+                
+                print('*' * 12)
+                print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
             else:
                 print('Papel gana a piedra');
                 print('La computadora ha ganado.');
+                computer_victories += 1;
+                
+                print('*' * 12)
+                print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
         elif user_option == 'papel':
             if computer_option == 'tijera':
                 print('Tijera gana a papel');
                 print('La computadora ha ganado.');
+                computer_victories += 1;
+                
+                print('*' * 12)
+                print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
             else:
                 print('Papel gana a piedra');
                 print('El o la usuario ha ganado.');
+                user_victories += 1;
+                
+                print('*' * 12)
+                print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
         elif user_option == 'tijera':
             if computer_option == 'papel':
                 print('Tijera gana a papel');
                 print('El o la usuario ha ganado.');
+                user_victories += 1;
+                
+                print('*' * 12)
+                print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
             else:
                 print('Piedra gana a tijera');
                 print('La computadora ha ganado.');
+                computer_victories += 1;
+                
+                print('*' * 12)
+                print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+                
+        if user_victories == 2:
+            print('Una maravillosa vicotria de la o el usuario.');
+            break
+        
+        if computer_victories == 2:
+            print('Una maravillosa vicotria de la compu.');
+            break
+        
     else:
-        print('Gracias por jugar.')
+        
+        print('Esa opción no es valida, escoge de nuevo (tienes 2 oportunidades más).')
+        
+        i = 0
+        while i < 2:
+            i += 1
+            user_option = input('Elige piedra, papel o tijera: ').lower();
+            
+            if user_option in game_options:
+                break
+            
+        if user_option in game_options:
+            computer_option = rd.choice(game_options)
+        
+            print(f'Has escogido {user_option}')
+            print(f'La computadora ha escogido {computer_option}')
+            
+            if user_option == computer_option:
+                print('Esto es un empate, seleccionaron la misma opcion');
+            elif user_option == 'piedra':
+                if computer_option == 'tijera':
+                    print('Piedra gana a tijera');
+                    print('El o la usuario ha ganado.');
+                    user_victories += 1;
+                    
+                    print('*' * 12)
+                    print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+                else:
+                    print('Papel gana a piedra');
+                    print('La computadora ha ganado.');
+                    computer_victories += 1;
+                    
+                    print('*' * 12)
+                    print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+            elif user_option == 'papel':
+                if computer_option == 'tijera':
+                    print('Tijera gana a papel');
+                    print('La computadora ha ganado.');
+                    computer_victories += 1;
+                    
+                    print('*' * 12)
+                    print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+                else:
+                    print('Papel gana a piedra');
+                    print('El o la usuario ha ganado.');
+                    user_victories += 1;
+                    
+                    print('*' * 12)
+                    print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+            elif user_option == 'tijera':
+                if computer_option == 'papel':
+                    print('Tijera gana a papel');
+                    print('El o la usuario ha ganado.');
+                    user_victories += 1;
+                    
+                    print('*' * 12)
+                    print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+                else:
+                    print('Piedra gana a tijera');
+                    print('La computadora ha ganado.');
+                    computer_victories += 1;
+                    
+                    print('*' * 12)
+                    print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+        else:
+            print('Gracias por jugar.')
+    
+        if user_victories == 2:
+            print('Una maravillosa vicotria de la o el usuario.');
+            break
+        
+        if computer_victories == 2:
+            print('Una maravillosa vicotria de la compu.');
+            break
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,23 +1,160 @@
-matriz = [
-    [1,2,3], 
-    [4,5,6], 
-    [7,8,9]
-]
+import random as rd
 
-print(matriz[0])
-# [1,2,3]
+print('Este es el juego de piedra, papel o tijera.');
 
-print(matriz[0][1])
-# 2
+game_options = ('piedra', 'papel', 'tijera');
 
-# es importante no repetir variables en los ciclos anidados
-for i in matriz:
-    print(i)
-    for j in i:
-        print(j)
+rondas = 0
+
+user_victories = 0
+computer_victories = 0
+
+while True:
+    
+    rondas += 1
+    
+    print('*' * 12)
+    print(f'Ronda {rondas}')
+    print('*' * 12)
+    
+    user_option = input('Elige piedra, papel o tijera: ').lower();
+
+
+    if user_option in game_options:
         
-# para hacer una denominacion de variables mas descriptiva, podemos hacer
-
-for row in matriz:
-    for column in row:
-        print(column)
+        computer_option = rd.choice(game_options)
+        
+        print(f'Has escogido {user_option}')
+        print(f'La computadora ha escogido {computer_option}')
+        
+        if user_option == computer_option:
+            print('Esto es un empate, seleccionaron la misma opcion');
+        elif user_option == 'piedra':
+            if computer_option == 'tijera':
+                print('Piedra gana a tijera');
+                print('El o la usuario ha ganado.');
+                user_victories += 1;
+                
+                print('*' * 12)
+                print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+            else:
+                print('Papel gana a piedra');
+                print('La computadora ha ganado.');
+                computer_victories += 1;
+                
+                print('*' * 12)
+                print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+        elif user_option == 'papel':
+            if computer_option == 'tijera':
+                print('Tijera gana a papel');
+                print('La computadora ha ganado.');
+                computer_victories += 1;
+                
+                print('*' * 12)
+                print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+            else:
+                print('Papel gana a piedra');
+                print('El o la usuario ha ganado.');
+                user_victories += 1;
+                
+                print('*' * 12)
+                print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+        elif user_option == 'tijera':
+            if computer_option == 'papel':
+                print('Tijera gana a papel');
+                print('El o la usuario ha ganado.');
+                user_victories += 1;
+                
+                print('*' * 12)
+                print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+            else:
+                print('Piedra gana a tijera');
+                print('La computadora ha ganado.');
+                computer_victories += 1;
+                
+                print('*' * 12)
+                print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+                
+        if user_victories == 2:
+            print('Una maravillosa vicotria de la o el usuario.');
+            break
+        
+        if computer_victories == 2:
+            print('Una maravillosa vicotria de la compu.');
+            break
+        
+    else:
+        
+        print('Esa opción no es valida, escoge de nuevo (tienes 2 oportunidades más).')
+        
+        i = 0
+        while i < 2:
+            i += 1
+            user_option = input('Elige piedra, papel o tijera: ').lower();
+            
+            if user_option in game_options:
+                break
+            
+        if user_option in game_options:
+            computer_option = rd.choice(game_options)
+        
+            print(f'Has escogido {user_option}')
+            print(f'La computadora ha escogido {computer_option}')
+            
+            if user_option == computer_option:
+                print('Esto es un empate, seleccionaron la misma opcion');
+            elif user_option == 'piedra':
+                if computer_option == 'tijera':
+                    print('Piedra gana a tijera');
+                    print('El o la usuario ha ganado.');
+                    user_victories += 1;
+                    
+                    print('*' * 12)
+                    print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+                else:
+                    print('Papel gana a piedra');
+                    print('La computadora ha ganado.');
+                    computer_victories += 1;
+                    
+                    print('*' * 12)
+                    print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+            elif user_option == 'papel':
+                if computer_option == 'tijera':
+                    print('Tijera gana a papel');
+                    print('La computadora ha ganado.');
+                    computer_victories += 1;
+                    
+                    print('*' * 12)
+                    print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+                else:
+                    print('Papel gana a piedra');
+                    print('El o la usuario ha ganado.');
+                    user_victories += 1;
+                    
+                    print('*' * 12)
+                    print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+            elif user_option == 'tijera':
+                if computer_option == 'papel':
+                    print('Tijera gana a papel');
+                    print('El o la usuario ha ganado.');
+                    user_victories += 1;
+                    
+                    print('*' * 12)
+                    print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+                else:
+                    print('Piedra gana a tijera');
+                    print('La computadora ha ganado.');
+                    computer_victories += 1;
+                    
+                    print('*' * 12)
+                    print(f'El marcador es {user_victories} punto(s) a favor del usario y {computer_victories} punto(s) a favor de la compu.')
+        else:
+            print('Gracias por jugar.')
+    
+        if user_victories == 2:
+            print('Una maravillosa vicotria de la o el usuario.');
+            break
+        
+        if computer_victories == 2:
+            print('Una maravillosa vicotria de la compu.');
+            break
