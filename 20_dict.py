cc4efbcf902af9b1bd8fa50f440eb8a102fee85c
dr1602 <david.arangelg@gmail.com>
2024-06-14 12:59:22 -0600

    Ejercicio Diccionarios 14jun24

--- 20_dict.py
+++ 20_dict.py
@@ -47,3 +47,28 @@ print(character)
 print(f'Los items son {character.items()}')
 print(f'Los keys son {character.keys()}')
 print(f'Los values son {character.values()}')
+
+
+# EJERCICIO DE CLASE
+
+person = {
+    'name': 'Nicolas',
+    'lastName': 'Molina',
+    'age': 29
+}
+
+person_social_media = {
+    'twitter': '@nicobytes'
+}
+
+person.update(person_social_media)
+# print(person)
+
+person['name'] = 'Felipe'
+# print(person)
+
+del person['age']
+# print(person)
+
+print(list(person.keys()))
+print(list(person.values()))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,49 +1,21 @@
-character = {
-    'name': 'Monkey',
-    'last_name': 'D. Luffy',
-    'pseudonym': ['Mugiwara', 'Fifthe Emperor', 'Joy Boy' ],
-    'race': 'human',
-    'affiliations': [
-        'Dadan Family (formerly)',
-        'Straw Hat Pirates',
-        'Straw Hat Grand Fleet', 
-        'Ninja-Pirate-Mink-Samurai Alliance (dissolved)',
-        'The Four Emperors'
-    ],
-    'bounties': {
-        'First (Belly)': 30000000,
-        'Second (Belly)': 100000000,
-        'Third (Belly)': 300000000,
-        'Fourth (Belly)': 400000000,
-        'Fifth (Belly)': 500000000,
-        'Sixth (Belly)': 1500000000,
-        'Current (Belly)': 3000000000,
-    },
-    'age': 17,
-    'height (cm)': 172
+person = {
+    'name': 'Nicolas',
+    'lastName': 'Molina',
+    'age': 29
 }
 
-print(character)
+person_social_media = {
+    'twitter': '@nicobytes'
+}
 
-character['age'] = 19;
-character['height (cm)'] += 2;
+person.update(person_social_media)
+# print(person)
 
-print(character)
-print(character.get('age'))
-print(character.get('height (cm)'))
+person['name'] = 'Felipe'
+# print(person)
 
-character['affiliations'].append('Pirate King')
-print(character.get('affiliations'))
+del person['age']
+# print(person)
 
-del character['race']
-print(character)
-
-character.pop('affiliations')
-print(character)
-
-# character.pop()
-# con dicitionarios, pop requiere forzosamente la clave del elemento a eliminar
-
-print(f'Los items son {character.items()}')
-print(f'Los keys son {character.keys()}')
-print(f'Los values son {character.values()}')
+print(list(person.keys()))
+print(list(person.values()))
