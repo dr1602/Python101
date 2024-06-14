e37749f2e5596f10e0dfde24bbb4bb13e25d500c
dr1602 <david.arangelg@gmail.com>
2024-06-14 12:38:53 -0600

    Diccionario Insercion y Actualizacion 14jun24

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,27 +1,49 @@
-my_dict = {}
-
-print(type(my_dict))
-print(my_dict)
-
-my_dict = {
-    'name': 'Mario Alberto',
-    'last_name': 'Lee Brown',
-    'age': 34  
+character = {
+    'name': 'Monkey',
+    'last_name': 'D. Luffy',
+    'pseudonym': ['Mugiwara', 'Fifthe Emperor', 'Joy Boy' ],
+    'race': 'human',
+    'affiliations': [
+        'Dadan Family (formerly)',
+        'Straw Hat Pirates',
+        'Straw Hat Grand Fleet', 
+        'Ninja-Pirate-Mink-Samurai Alliance (dissolved)',
+        'The Four Emperors'
+    ],
+    'bounties': {
+        'First (Belly)': 30000000,
+        'Second (Belly)': 100000000,
+        'Third (Belly)': 300000000,
+        'Fourth (Belly)': 400000000,
+        'Fifth (Belly)': 500000000,
+        'Sixth (Belly)': 1500000000,
+        'Current (Belly)': 3000000000,
+    },
+    'age': 17,
+    'height (cm)': 172
 }
 
-print(my_dict)
-print(len(my_dict))
+print(character)
 
-print(my_dict['name'])
-print(my_dict['age'])
-# print(my_dict['profesion'])
-# error
+character['age'] = 19;
+character['height (cm)'] += 2;
 
-print(my_dict.get('last_name'))
-print(my_dict.get('profesion'))
-# none
+print(character)
+print(character.get('age'))
+print(character.get('height (cm)'))
 
-print('profesion' in my_dict)
-# false
-print('age' in my_dict)
-# true
+character['affiliations'].append('Pirate King')
+print(character.get('affiliations'))
+
+del character['race']
+print(character)
+
+character.pop('affiliations')
+print(character)
+
+# character.pop()
+# con dicitionarios, pop requiere forzosamente la clave del elemento a eliminar
+
+print(f'Los items son {character.items()}')
+print(f'Los keys son {character.keys()}')
+print(f'Los values son {character.values()}')
